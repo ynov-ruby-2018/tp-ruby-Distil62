4ea705b9e916201af8d5a506b2a883ecccf8368f
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 16:09:21 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+ # Manque la liaison avec les commentaires (has_many) !
 end
