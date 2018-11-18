0277944661a57880e4d82ae991a1ec4e847041dc
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 16:14:25 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'golden_book#index'
-  resources :golden_book
+  resources :golden_book # only: [:index, :create] aurait été préférable, 5 routes existent mais ne servent à rien
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
